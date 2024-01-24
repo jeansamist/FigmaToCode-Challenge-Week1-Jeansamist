bcb58a1350f40faee082803fa8ae979f676fae3e
BAHA Ephraim <jeansamist@gmail.com>
2024-01-24 18:04:21 +0100

    Adding my link

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import me from "./assets/images/me.png";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+    <a
+      href="https://jeansamist.vercel.app/"
+      className="fixed z-50 bottom-4 right-4"
+    >
+      <img
+        src={me}
+        alt="Jeansamist"
+        className="w-12 aspect-square object-cover rounded-full border-grass border-4 shadow-xl opacity-70 hover:opacity-100"
+      />
+    </a>
+  </React.StrictMode>
+);
